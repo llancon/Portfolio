ef72d63da3b9f481f4afa0f665749998a85e5095
llancon <rdzalb@gmail.com>
2017-07-14 09:41:55 -0400

    Adds animation to name deletes unneccary files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-// ticker
 
-$(document).ready(function() {
-	$('.ticker').typer(['full-stack', 'back-end', 'rails']);
-});
+var str = "<h1>Luis Lancon</h1><h2>Web Developer</h2>",
+    i = 0,
+    isTag,
+    text;
+
+ (function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 150);
+}());
